642b7e5759920af4b0dc0f2a772fb0169a811e4e
Paul Boyle <pboyle17@gmail.com>
2023-12-03 22:38:57 -0500

    grabbing all unique symbols

--- day3/Day3Part1.java
+++ day3/Day3Part1.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day3Part1 {
   public static void main(String[] args) throws IOException {
@@ -12,17 +14,27 @@ public class Day3Part1 {
     ArrayList<ArrayList<String>> engineSchematic = new ArrayList<>();
     ArrayList<Integer> symbolCoordinates = new ArrayList<Integer>();
     ArrayList<Integer> partNumbers = new ArrayList<Integer>();
+    Set<Character> uniqueSymbols = new HashSet<>();
 
     input.lines().forEach(line -> {
-      engineSchematic.add(new ArrayList<>(Arrays.asList(line.split(""))));
+      ArrayList<String> lineList = new ArrayList<>(Arrays.asList(line.split("")));
+
+      engineSchematic.add(lineList);
+
+      lineList.forEach(character -> {
+        if (!character.contains(".") && !character.matches("\\d")) {
+          uniqueSymbols.add(character.toCharArray()[0]);
+        }
+      });
     });
 
     for (int column = 0; column < engineSchematic.size(); column++) {
       for (int row = 0; row < engineSchematic.get(row).size(); row++) {
-
+        System.out.println("inside loopception");
       }
     }
 
+    System.out.println(uniqueSymbols);
     System.out.println("DONE!");
   }
 }
