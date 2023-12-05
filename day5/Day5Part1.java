c01a3d6e9a396fdc634d836856b9cd4e7595bcb1
Paul Boyle <pboyle17@gmail.com>
2023-12-05 01:18:10 -0500

    wip

--- day5/Day5Part1.java
+++ day5/Day5Part1.java
@@ -3,14 +3,28 @@ package day5;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Day5Part1 {
   public static void main(String[] args) throws IOException {
     String input = Files.readString(Path.of("./day5/day5.txt"));
+    ArrayList<Integer> seeds = getSeeds(input);
+  }
 
-    input.lines().forEach(line -> {
-      System.out.println(line);
-    });
+  private static ArrayList<Integer> getSeeds(String input) {
+    String seedsString = input.lines().filter(s -> s.contains("seeds")).collect(Collectors.joining(""));
+
+    List<Integer> seedsList = Arrays.stream(seedsString.split(":")[1].split(" "))
+                                    .filter(s -> !s.isEmpty())
+                                    .collect(Collectors.toList())
+                                    .stream()
+                                    .map(num -> Integer.parseInt(num));
+
+    return new ArrayList<Integer>(seedsList);
   }
 
 
